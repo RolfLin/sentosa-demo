6b86f4c15c124316e03edb737b0a271f7420c36a
Li Wang <wangli1426@gmail.com>
2016-12-30 14:48:35 +0800

    login is implemented

---
+++ src/main/java/edu/illinois/adsc/sentosa/rest/Login.java
@@ -0,0 +1,22 @@
+package edu.illinois.adsc.sentosa.rest;
+
+import edu.illinois.adsc.sentosa.query.naive.NaiveQueryImpl;
+import org.json.JSONObject;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by robert on 28/12/16.
+ */
+@Path("login")
+public class Login {
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getFlow(
+            @FormParam("name") String name,
+            @FormParam("password") String password) {
+        System.out.println(String.format("Name: %s, Password: %s", name, password));
+        return "Success!";
+    }
+}
